32f192497a8c1b871ae42c7d7d04b88dd6c4fe49
SRT <essarti_abdelouahid@hotmail.com>
2024-04-04 21:01:36 +0200

    Styling changes

--- src/views/StadionView.vue
+++ src/views/StadionView.vue
@@ -21,17 +21,37 @@ onMounted(async () => {
 <template>
   <div id="container" v-if="stadion">
     <h1 class="text-center text-3xl mt-5">{{ stadion.name }}</h1>
-    <div class="m-9 flex flex-row flex-wrap">
+    <div class="m-9 flex flex-row flex-wrap justify-around">
       <img :src="stadion.image" class="h-48 rounded-md m-4" />
-      <ul class="text-center flex flex-col justify-center items-center gap-3">
-        <li>Address: {{ stadion.address }}</li>
-        <li>City: {{ stadion.city }}</li>
-        <li>Capacity: {{ stadion.capacity }}</li>
-        <li>Surface: {{ stadion.surface }}</li>
-      </ul>
+      <div class="flex justify-center items-center">
+        <table class="text-center table-auto">
+          <tr>
+            <th>Address:</th>
+            <td>{{ stadion.address }}</td>
+          </tr>
+          <tr>
+            <th>City:</th>
+            <td>{{ stadion.city }}</td>
+          </tr>
+          <tr>
+            <th>Capacity:</th>
+            <td>{{ stadion.capacity.toLocaleString('en') }}</td>
+          </tr>
+          <tr>
+            <th>Surface:</th>
+            <td>{{ stadion.surface }}</td>
+          </tr>
+          <tr></tr>
+        </table>
+      </div>
     </div>
   </div>
   <div class="" v-else>
     <h1>Loading...</h1>
   </div>
 </template>
+<style scoped>
+td {
+  margin-left: 1rem;
+}
+</style>
